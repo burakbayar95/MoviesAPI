77c479f9ea5098f2e9acaaa9ccbfd0ce34dbc5dc
Burak <95burakbayar@gmail.com>
2021-07-05 23:25:54 +0300

    Start

---
+++ Movies/Movies.Models/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Movies.Models
+{
+    public class Genre : IEntity // tür
+
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı boş geçilemez")]
+        public int Name { get; set; }
+
+        public virtual IList<Movie> Movies { get; set; }
+
+
+
+
+    }
+}
---
+++ Movies/Movies.Models/MovieActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies.Models
+{
+   public class MovieActor
+    {
+        public int ActorId { get; set; } //fk?
+        public int MovieId { get; set; }
+
+        public Actor Actor { get; set; }
+        public Movie Movie { get; set; }
+
+    }
+}
